3eb8e2126a5f14fe4f5f48141fae0320cf0ddeeb
Elvio Martinelli <emartinelli@protonmail.com>
2022-08-11 18:06:08 -0300

    Add revendedor validation

--- app/controllers/revendedor.py
+++ app/controllers/revendedor.py
@@ -2,14 +2,18 @@ from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
 
 from app.controllers.dependencies import get_db
+from app.repository.revendedor import DuplicateRevendedorException
 from app.schemas.revendedor import RevendedorIn, RevendedorOut
 from app.services.revendedor import RevendedorService
 
 router = APIRouter()
 
 
-@router.post("/", response_model=RevendedorOut)
+@router.post("/", response_model=RevendedorOut, status_code=201)
 def create_revendedor(revendedor_in: RevendedorIn, db: Session = Depends(get_db)):
     service = RevendedorService(db)
 
-    return service.create(revendedor_in)
+    try:
+        return service.create(revendedor_in)
+    except DuplicateRevendedorException:
+        raise HTTPException(status_code=422, detail="Revendedor já cadastrado")
