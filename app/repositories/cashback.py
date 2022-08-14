e22bb1df66f93589915c344fecc425e645ca43e0
Elvio Martinelli <emartinelli@protonmail.com>
2022-08-14 18:38:51 -0300

    Fxi create_cashback_criterios script

--- app/repositories/cashback.py
+++ app/repositories/cashback.py
@@ -1,14 +1,18 @@
+from decimal import Decimal
 from sqlalchemy.orm import Session
 
 from app.exceptions.revendedor import RevendedorNotFoundException
 from app.infra.clients.cashback import CashbackClient
 from app.repositories.revendedor import RevendedorRepository
 from app.schemas.cashback import CashbackAcumuladoOut
+from app.models.cashback import CashbackCriterio
+from psycopg2.extras import NumericRange
 
 
 class CashbackRepository:
     def __init__(self, db: Session) -> None:
         self.client = CashbackClient()
+        self.db = db
         self.revendedor_repository = RevendedorRepository(db)
 
     def get_cashback_acumulado(self, cpf: str) -> CashbackAcumuladoOut:
@@ -22,3 +26,15 @@ class CashbackRepository:
             revendedor=RevendedorRepository.map_model_to_schema(revendedor),
             cashback_acumulado=self.client.get_cashback_acumulado(cpf),
         )
+
+    def create_criterio(
+        self, intervalo: NumericRange, porcentagem_de_cashback=Decimal
+    ) -> CashbackCriterio:
+        cash_back_criterio = CashbackCriterio(
+            intervalo=intervalo,
+            porcentagem_de_cashback=porcentagem_de_cashback,
+        )
+        self.db.add(cash_back_criterio)
+        self.db.commit()
+        self.db.refresh(cash_back_criterio)
+        return cash_back_criterio
